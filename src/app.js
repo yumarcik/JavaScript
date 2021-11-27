2e139a053e8d106b970bff959a0e034505d40471
Yağmur Polat <yamu_plt@hotmail.com>
2021-11-27 18:40:00 +0300

    homework1

--- src/app.js
+++ src/app.js
@@ -1,33 +1,96 @@
-console.log("Merhaba Kodlama.io")
+// console.log("Merhaba Kodlama.io")
 
-// JS type safe değildir
+// // JS type safe değildir
 
-let dolarBugun = 9.30
+// let dolarBugun = 9.30
 
-let dolarDun = 9.20
-dolarDun = "9.20"
+// let dolarDun = 9.20
+// dolarDun = "9.20"
 
-{
-    let dolarDun = 9.10
+// {
+//     let dolarDun = 9.10
+// }
+
+// console.log(dolarDun)
+
+// const euroDun = 11.2
+// // euroDun = 11
+
+// console.log(euroDun)
+
+// //array
+// //camelCasing
+// //PascalCasing
+// let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi", "Özel Konut Kredisi"]
+
+// console.log("<ul>")
+
+// for (let i = 0; i < konutKredileri.length; i++) {
+//         console.log("<li>" + konutKredileri[i] + "</li>")
+// }
+// console.log("</ul>")
+
+// console.log(konutKredileri)
+
+let sayi1 = 10
+let student = {id: 1, name: "Yağmur"}
+//console.log(student)
+
+function save(puan=10, ogrenci) {
+    console.log(ogrenci.name + " : " + puan)
 }
 
-console.log(dolarDun)
+save(undefined, student);
 
-const euroDun = 11.2
-// euroDun = 11
+let students = ["Yağmur Polat", "Can Öztürk", "Engin Demiroğ", "Halit Kalaycı"]
 
-console.log(euroDun)
+// console.log(students)
 
-//array
-//camelCasing
-//PascalCasing
-let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi", "Özel Konut Kredisi"]
+let students2 = [student, {id: 2, name: "Can"}, "Ankara", {city: "İstanbul"}]
+//console.log(students2)
 
-console.log("<ul>")
-
-for (let i = 0; i < konutKredileri.length; i++) {
-        console.log("<li>" + konutKredileri[i] + "</li>")
+//rest
+//params C#
+//varArgs java
+let showProducts = function (id,...products) {
+    console.log(id)
+    console.log(products[0])
 }
-console.log("</ul>")
+//console.log(typeof showProducts)
+//showProducts(10, ["Elma", "Armut", "Karpuz"])
 
-console.log(konutKredileri)
+//spread 
+let points = [1,2,3,4,50,4,60,14]
+//console.log(...points)
+//console.log(Math.max(...points))
+
+//console.log(..."ABC", "D",..."EFG", "H")
+
+// let ask = "CAN ÖZTÜRK"
+// console.log(ask)
+// console.log(...ask)
+
+// Destructuring
+let populations = [10000, 20000, 30000, [40000,100000]]
+let [small, medium, high, [veryHigh, maximum]] = populations
+console.log(small) 
+console.log(medium) 
+console.log(high) 
+console.log(veryHigh)
+console.log(maximum)
+
+function someFunction([small1], number) {
+    console.log(small1)
+}
+
+someFunction(populations)
+
+let category = {id:1, name: "içecek"}
+console.log(category.id)
+console.log(category["name"])
+
+let {id, name} = category
+console.log(id)
+console.log(name)
+
+//Redux
---
+++ src/homeWork/homeWork1.js
@@ -0,0 +1,85 @@
+// 1st - Finding the prime numbers
+function findPrime(...numbers) {
+    let isPrime = true
+    for(let i = 0; i < numbers.length; i++) {
+        isPrime = true
+        if(numbers[i] == 0 || numbers[i] == 1) {
+            console.log(numbers[i] + " asal sayı değildir.")
+            continue
+        }
+        for(let j = 2; j <= numbers[i] / 2; j++){
+            if(numbers[i] % j == 0) {
+                isPrime = false                
+                break
+            }
+        }
+        if(isPrime == true) {
+            console.log(numbers[i] + " asal sayıdır.")
+        }
+        else {
+            console.log(numbers[i] + " asal sayı değildir.")
+        }
+    }
+}
+
+findPrime(1,2,19,4,2,14, 93, 87, 89)
+
+// 2nd - Arkadas Sayilar
+function arkadasSayiMi(num1, num2) {
+    let total1 = 0
+    let total2 = 0
+    for(let i = 1; i < num1; i++) {
+        if(num1 % i == 0) {
+            total1 = total1 + i
+        }
+    }
+    for(let i = 1; i < num2; i++) {
+        if(num2 % i == 0) {
+            total2 = total2 + i
+        }
+    }
+    if(num1 == total2 && num2 == total1) {
+        console.log(num1 + " ve " + num2 + " Arkadaş Sayılardır.")
+    }
+    else {
+        console.log(num1 + " ve " + num2 + " Arkadaş Sayılar Değildir.")
+    }
+}
+
+arkadasSayiMi(1184, 1210)
+
+// 3rd - Mukemmel Sayilar
+function mukemmelSayiListele() {
+    for(let i = 1; i < 1000; i++) {
+        let total = 0
+        for(let j = 1; j <= i; j++) {
+            if(i % j == 0){
+                total += j
+            }
+        }
+        if (2 * i == total) {
+            console.log(i + " sayısı mükemmel sayıdır.")
+        }
+    }
+}
+
+mukemmelSayiListele()
+
+// 4th - Prime Sayilar
+function asalSayiListele() {
+    for(let i = 2; i < 1000; i++) {        
+        let asalMi = true
+        for(let j = 2; j < i; j++) {
+            
+            if(i % j == 0) {
+                asalMi = false
+            }            
+        }
+        if (asalMi == true) {
+            console.log(i + " sayısı asaldır")    
+        }
+    }
+    
+}
+asalSayiListele()
+
